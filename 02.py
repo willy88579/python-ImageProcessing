fcfe5556905ae7bf367cc47006d3a7077f7cadee
willy88579 <167272451+willy88579@users.noreply.github.com>
2024-05-08 11:43:28 +0800

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,7 @@
+import pytesseract
+from PIL import Image
+
+#pytesseract.pytesseract.tesseract_cmd = r"D:\Chung_Hua_University\2024\Tesseract-OCR\tesseract.exe"
+img = Image.open(r"word.jpg")
+#img.show()
+print(pytesseract.image_to_string(img, lang="chi_tra"))
