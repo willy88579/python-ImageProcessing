d604aba40478fb8fa8c53ae36a0625545cada33d
willy88579 <167272451+willy88579@users.noreply.github.com>
2024-05-08 15:20:52 +0800

    Delete 04.py

--- 04.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import pytesseract
-import numpy as np
-
-filename = '2_python-ocr.jpg'
-img1 = np.array(Image.open(filename))
-text = pytesseract.image_to_string(img1)
-print(text)
