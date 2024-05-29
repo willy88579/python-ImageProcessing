311d7183973e168cab8b2c5cb0810c519f2dfa2b
willy88579 <167272451+willy88579@users.noreply.github.com>
2024-05-29 12:06:40 +0800

    Add files via upload

---
+++ 04/03.py
@@ -0,0 +1,15 @@
+import tabula
+
+pdf_path = "data03.pdf"
+dfs = tabula.read_pdf(
+    pdf_path,
+    pages="1",
+    lattice=True,
+    pandas_options={"header": [0, 1]},
+    area=[0, 0, 50, 100],
+    relative_area=True,
+    multiple_tables=False,
+)
+
+print(dfs[0])
+dfs[0].to_csv('ok03.csv')
